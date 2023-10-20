a4d346a1e4a43150c9e101d5f1de092f2a6f3870
suji5262 <suji5262@gmail.com>
2023-10-20 19:47:03 +0900

    :sparkles: SampleVO 파일 add

---
+++ boot01/boot01/src/main/java/org/zerock/domain/SampleVO.java
@@ -0,0 +1,13 @@
+package org.zerock.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(exclude = {"val3"})
+public class SampleVO {
+
+    private String val1;
+    private String val2;
+    private String val3;
+}
