9c9c9cd260d4ef38d6d0e661ac6ba52ed92fc70c
suji5262 <suji5262@gmail.com>
2023-10-24 16:02:30 +0900

    :sparkles: Board file add

---
+++ boot02/boot02/src/main/java/org/zerock/domain/Board.java
@@ -0,0 +1,30 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "tbl_boards")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+    private String writer;
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp regdate; // LocalDateTime
+    @UpdateTimestamp
+    private Timestamp updatedate; // LocalDateTime
+}
