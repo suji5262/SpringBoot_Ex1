f83d5884bfd34a1cb90832a9bbb1331b822ee556
suji5262 <suji5262@gmail.com>
2023-10-30 02:15:33 +0900

    :sparkles: PDSFile file 추가

---
+++ boot04/boot04/src/main/java/org/zerock/domain/PDSFile.java
@@ -0,0 +1,23 @@
+package org.zerock.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "tbl_pdsfiles")
+@EqualsAndHashCode(of = "fno")
+public class PDSFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fno;
+
+    private String pdsfile;
+}
