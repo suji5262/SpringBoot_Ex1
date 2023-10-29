91ab681ffa2a88fff618e7475c462738c0f495c6
suji5262 <suji5262@gmail.com>
2023-10-30 02:15:11 +0900

    :sparkles: PDSBoard file 추가

---
+++ boot04/boot04/src/main/java/org/zerock/domain/PDSBoard.java
@@ -0,0 +1,29 @@
+package org.zerock.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(exclude = "files")
+@Entity
+@Table(name = "tbl_pds")
+@EqualsAndHashCode(of = "pid")
+public class PDSBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pid;
+
+    private String pname;
+    private String pwriter;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pdsno")
+    private List<PDSFile> files;
+}
