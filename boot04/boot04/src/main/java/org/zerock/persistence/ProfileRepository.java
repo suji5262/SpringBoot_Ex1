c063046ab8483a57e2db7080539c8a6a06c1576c
suji5262 <suji5262@gmail.com>
2023-10-30 02:15:46 +0900

    :sparkles: ProfileRepository file 추가

---
+++ boot04/boot04/src/main/java/org/zerock/persistence/ProfileRepository.java
@@ -0,0 +1,7 @@
+package org.zerock.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.Profile;
+
+public interface ProfileRepository extends CrudRepository<Profile, Long> {
+}
